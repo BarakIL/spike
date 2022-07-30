8201bcf0bc03bc1937e6636750f95092965cd8b0
BarakIL <110297739+BarakIL@users.noreply.github.com>
2022-07-30 18:22:30 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spike Easy Scanner
+
+
+## spike is fast scanner can help your scan all range ip
+## by only country code wiyh multi-threading
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='spike',
+  version='0.0.1',
+  description='Spike Powerful Ranger Scanner',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='ClickCyber',
+  author_email='',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Spike', 
+  packages=find_packages(),
+  install_requires=['requests==2.28.1 '] 
+)
